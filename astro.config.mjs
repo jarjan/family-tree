77bdf4f2613a22acde63ff2e74fb28575734288e
Zharzhan Kulmyrza <hey@jarjan.xyz>
2026-04-19 19:34:30 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jarjan.github.io',
+  base: '/family-tree',
   integrations: [preact()]
 });
